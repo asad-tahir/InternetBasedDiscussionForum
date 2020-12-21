1be139692aaabc1cddd7f9e19a967bcfa5cfbca3
assadtahir <asad.ch1996@gmail.com>
2020-12-21 17:45:56 +0500

    Add project files.

---
+++ Migrations/202012211154032_NameAndIsActiveColumn.cs
@@ -0,0 +1,20 @@
+﻿namespace InternetBasedDiscussionForum.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class NameAndIsActiveColumn : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.AspNetUsers", "Name", c => c.String());
+            AddColumn("dbo.AspNetUsers", "IsActive", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.AspNetUsers", "IsActive");
+            DropColumn("dbo.AspNetUsers", "Name");
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(InternetBasedDiscussionForum.Startup))]
+namespace InternetBasedDiscussionForum
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
