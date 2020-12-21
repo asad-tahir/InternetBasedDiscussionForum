d437859b2cb0653309a6c8a859f13bfe33788d7f
assadtahir <asad.ch1996@gmail.com>
2020-12-21 23:36:36 +0500

    21 dec flat

---
+++ Scripts/app.admin.pendingusers.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+    $.ajax({
+        url: '/Api/Users/pendingusers',
+        method: 'GET',
+        contentType: 'json',
+        success: function (result) {
+            result.forEach(function (temp) {
+                var userRow = $('#user-data-row').clone();
+                userRow.removeAttr('style');
+                userRow.removeAttr('id');
+                var name = userRow.find('#name');
+                name.text(temp.Name);
+                name.removeAttr('id');
+                var email = userRow.find('#email');
+                email.text(temp.Email);
+                email.removeAttr('id');
+                var actions = userRow.find('#actions');
+                actions.removeAttr('id');
+                $('tbody').append(userRow);
+            });
+        }
+    });
+});
